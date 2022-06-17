90e976a7dd676c2d9059452a6d4c7116bc951431
LeninYair <l.camacho@intsuperior.edu.ec>
2022-06-16 19:12:08 -0500

    tres  numeros sumados

--- app/src/main/java/xyz/yoandroide/cambiartextoconboton/MainActivity.java
+++ app/src/main/java/xyz/yoandroide/cambiartextoconboton/MainActivity.java
@@ -18,22 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         EditText txtn1;
         EditText txtn2;
+        EditText txtn3;
         Button btnSuma;
         TextView txtvResultado;
 
         txtn1=(EditText) findViewById(R.id.txtn1);
         txtn2=(EditText) findViewById(R.id.txtn2);
+        txtn3=(EditText) findViewById(R.id.txtn3);
         btnSuma=(Button) findViewById(R.id.btnSuma);
         txtvResultado=(TextView) findViewById(R.id.textvResultado);
 
         btnSuma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int numeroUno, numeroDos;
+                int numeroUno, numeroDos, numeroTres;
                 int resultado=0;
                 numeroUno= Integer.parseInt(txtn1.getText().toString());
                 numeroDos= Integer.parseInt(txtn2.getText().toString());
-                resultado=numeroUno+numeroDos;
+                numeroTres= Integer.parseInt(txtn3.getText().toString());
+                resultado=numeroUno+numeroDos+numeroTres;
                 txtvResultado.setText("el resultado es "+resultado);
                 Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
             }
